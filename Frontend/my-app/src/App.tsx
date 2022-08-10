4cf202130934e3248fc62d9733effac815fb752f
SyedKaz00 <skaz269@aucklanduni.ac.nz>
2022-08-10 15:57:01 +1200

    Initial Data Entry form

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,25 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Rating,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+
 
 function App() {
+  const [curr, setCurr] = React.useState('bitcoin');
+  const [date, setDate] = React.useState('');
+  const [amount, setAm] = React.useState('');
+
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurr((event.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = () => {
+    todaysDate()
+    
+  }
+
+    //This function will return todays Date to use as the max in the date field
+  const todaysDate = () =>{
+    let today = new Date();
+    return (today.getFullYear()+"-"+today.toLocaleString("en-US", { month: '2-digit' })+"-"+today.toLocaleString("en-US", { day : '2-digit'}))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box>
+      <div>Calculate Crypto Gains/Losses</div>
+    <Box sx={{
+      width: '50%',
+      height: '10%',
+      backgroundColor: 'silver',
+      borderColor: 'pink',
+    }}>
+      <form>
+        <FormGroup
+          sx={{
+            padding: 2,
+            borderRadius: 2,
+            border: "1px solid",
+            borderColor: "primary.main",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FormLabel component="legend">Initial "Investment"</FormLabel>
+          <TextField
+            sx={{ paddingBottom: 2 }}
+            type="number"
+            name="initValue"
+            variant="outlined"
+            placeholder="$100"
+            value={amount}
+            InputProps={{ inputProps: { min: 0, max: 1000000000 } }}
+            onChange={(prop: any) => {
+              setAm(prop.target.value);
+            }}
+            required
+          />
+          <FormLabel component="legend">Select Currency</FormLabel>
+          <FormControl>
+            <FormLabel id="currency">Crypto Currencies</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="Crypto"
+              value={curr}
+              onChange={handleRadioChange}
+            >
+              <FormControlLabel value="bitcoin" control={<Radio />} label="Bitcoin" />
+              <FormControlLabel value="etherium" control={<Radio />} label="Ethereum" />
+            </RadioGroup>
+          </FormControl>
+
+          <Typography component="legend">Date Of Investment</Typography>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="dateInit"
+            value={date}
+            label=""
+            type="date"
+            fullWidth
+            variant="standard"
+            InputProps={{ inputProps: { max: todaysDate() } }}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <Button variant="outlined" onClick={handleSubmit}>Submit</Button>
+        </FormGroup>
+      </form>
+    </Box>
+    </Box>
   );
 }
 
